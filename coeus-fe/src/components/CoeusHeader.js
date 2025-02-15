85d48c3d765d4a8b6cd017f56dce8c0a94f1faf7
Alex Cholmsky <acholmsk@uwaterloo.ca>
2025-02-15 09:01:52 -0800

    coeus header

---
+++ coeus-fe/src/components/CoeusHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+  // Title at the top
+  const titleStyle = {
+    fontSize: '2rem',
+    textAlign: 'center',
+    marginBottom: '1rem'
+  };
+
+const CoeusHeader = () => {
+    return <h1 style={titleStyle}>Coeus</h1>
+}
+
+export default CoeusHeader;
